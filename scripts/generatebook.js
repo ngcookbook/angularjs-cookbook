ba5b6c0a52fd2ade750b33b3cbdf10248914debd
Sascha Brink <sascha.brink@gmail.com>
2013-10-22 11:54:20 +0200

    Zwischenstand

--- scripts/generatebook.js
+++ scripts/generatebook.js
@@ -8,23 +8,23 @@ var parts = ['mainmatter', 'directives', 'controllers', 'services',
 book.write('introduction.md\n\n');
 
 parts.forEach(function (part) {
-    console.log('recipes/parts/'+part+'.md');
-    book.write('recipes/parts/'+part+'.md\n');
+    console.log('recipes-parts/'+part+'.md');
+    book.write('recipes-parts/'+part+'.md\n');
 
-    fs.readdirSync('recipes/done').forEach(function (filename) {
+    fs.readdirSync('recipes-done').forEach(function (filename) {
         if (filename.indexOf(part+'-') === 0) {
             if (filename.indexOf('-q-') !== -1) { quickies += 1; }
-            console.log('recipes/done/'+filename);
-            book.write('recipes/done/'+filename+'\n');
+            console.log('recipes-done/'+filename);
+            book.write('recipes-done/'+filename+'\n');
             finished += 1;
         }
     });
 
-    fs.readdirSync('recipes/unfinished').forEach(function (filename) {
+    fs.readdirSync('recipes-unfinished').forEach(function (filename) {
         if (filename.indexOf(part+'-') === 0) {
             if (filename.indexOf('-q-') !== -1) { quickies += 1; }
-            console.log('recipes/unfinished/'+filename);
-            book.write('recipes/unfinished/'+filename+'\n');
+            console.log('recipes-unfinished/'+filename);
+            book.write('recipes-unfinished/'+filename+'\n');
             unfinished += 1;
         }
     });
